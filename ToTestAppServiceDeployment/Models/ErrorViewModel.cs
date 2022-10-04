541693f19eb9fb49bfe202c4598d80a77ebd9700
kishansindhe <80248684+kishansindhe@users.noreply.github.com>
2022-10-04 22:01:45 +0530

    Add project files.

---
+++ ToTestAppServiceDeployment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ToTestAppServiceDeployment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
